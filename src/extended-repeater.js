5d0b155328c43e4a3b94fddd9f5749691761fd34
Evgeniia <100534408+emoxowa@users.noreply.github.com>
2022-05-02 18:26:26 +0300

    extend-repeater complited

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let i = 1;
+  let j = 1;
+  let subAdd = '';
+  let subStr = '';
+  // преобразование типов
+  str = String(str);
+  addition = String(options.addition);
+  separator = String(options.separator);
+  additionSeparator = String(options.additionSeparator);
+  repeatTimes = Number(options.repeatTimes);
+  additionRepeatTimes = Number(options.additionRepeatTimes);
+  // значения по умолчанию
+  if (separator == "undefined") {separator = "+" };
+  if (additionSeparator == "undefined") {additionSeparator = "|";};
+  if (addition == "undefined") { addition = ""};
+
+  while (i < additionRepeatTimes) {
+    subAdd += addition + additionSeparator;
+    i++;
+  }
+  subAdd += addition;
+
+  while (j < repeatTimes) {
+    subStr += str + subAdd + separator;
+    j++;
+  }
+  subStr += str + subAdd;
+
+  return subStr;
 }
 
 module.exports = {
   repeater
 };
+
+// console.log(
+//   repeater("STRING", {
+//     repeatTimes: 3,
+//     separator: "**",
+//     addition: "PLUS",
+//     additionRepeatTimes: 3,
+//     additionSeparator: "00",
+//   })
+// );
+
+// console.log(repeater("la", { repeatTimes: 3 }));
