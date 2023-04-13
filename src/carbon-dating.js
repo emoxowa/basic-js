ac45646ba0e873a68d8b003f460c9f43a9d79c6e
Evgeniia <Kochetkovaev11@gmail.com>
2023-04-13 18:49:10 +0300

    Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -19,13 +19,15 @@ const HALF_LIFE_PERIOD = 5730;
  */
 function dateSample(sampleActivity) {
   const age = Math.ceil(
-    Math.log(MODERN_ACTIVITY / sampleActivity) / (Math.log(2)/ HALF_LIFE_PERIOD)
+    Math.log(MODERN_ACTIVITY / sampleActivity) /
+      (Math.log(2) / HALF_LIFE_PERIOD)
   );
-  if ( typeof sampleActivity !== "string" || (sampleActivity < 0 || sampleActivity > MODERN_ACTIVITY) || (!Number(sampleActivity))) {
-    return false;
-  } else {
-    return age;
-  }
+  return typeof sampleActivity !== "string" ||
+    sampleActivity < 0 ||
+    sampleActivity > MODERN_ACTIVITY ||
+    !Number(sampleActivity)
+    ? false
+    : age;
 }
 
 module.exports = {
