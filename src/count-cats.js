af1564dd0bd7e308bc9b0cc865dde3805d0d1a92
Evgeniia <Kochetkovaev11@gmail.com>
2023-04-13 18:42:09 +0300

    countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,16 +15,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function countCats(matrix) {
-  let number = 0;
-  const cat = "^^";
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] === cat) {
-        number += 1;
-      }
-    }
-  }
-  return number;
+  return matrix.flat().filter((i) => i == "^^").length;
 }
 
 module.exports = {
