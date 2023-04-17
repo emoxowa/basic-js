e6ac3a19cc47ff2e15f7194cfc2e26943f04a19c
Evgeniia <Kochetkovaev11@gmail.com>
2023-04-17 19:36:43 +0300

    vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let encryptedMessage = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        const messageIndex = this.alphabet.indexOf(message[i]);
+        const keyChar = key[keyIndex % key.length];
+        const keyCharIndex = this.alphabet.indexOf(keyChar);
+        const encryptedChar =
+          this.alphabet[(messageIndex + keyCharIndex) % this.alphabet.length];
+        encryptedMessage += encryptedChar;
+        keyIndex++;
+      } else {
+        encryptedMessage += message[i];
+      }
+    }
+
+    return this.isDirect
+      ? encryptedMessage
+      : encryptedMessage.split("").reverse().join("");
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let message = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (this.alphabet.includes(encryptedMessage[i])) {
+        const encryptedCharIndex = this.alphabet.indexOf(encryptedMessage[i]);
+        const keyChar = key[keyIndex % key.length];
+        const keyCharIndex = this.alphabet.indexOf(keyChar);
+        const messageChar =
+          this.alphabet[
+            (encryptedCharIndex - keyCharIndex + this.alphabet.length) %
+              this.alphabet.length
+          ];
+        message += messageChar;
+        keyIndex++;
+      } else {
+        message += encryptedMessage[i];
+      }
+    }
+
+    return this.isDirect ? message : message.split("").reverse().join("");
   }
 }
 
